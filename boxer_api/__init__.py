7b76d8ed401838b6c263b5c0dd407d11587c9814
yoshiri <yoshiyoshidetteiu@gmail.com>
2026-04-18 11:02:27 +0900

    Add single-frame boxer inference engine API

--- boxer_api/__init__.py
+++ boxer_api/__init__.py
@@ -2,10 +2,13 @@
 
 from .adapters import frame_input_from_datum
 from .config import BoxerConfig, DetectorConfig, PipelineConfig, TrackingConfig
+from .engine import BoxerInferenceEngine, BoxerInferenceRequest
 from .types import Detection2D, Detection3D, FrameInput, FrameResult, PipelineResult, Track3D
 
 __all__ = [
     "BoxerConfig",
+    "BoxerInferenceEngine",
+    "BoxerInferenceRequest",
     "DetectorConfig",
     "PipelineConfig",
     "TrackingConfig",
